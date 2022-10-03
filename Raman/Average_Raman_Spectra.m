f75567dd84021c90d0b2763b8d2b5219e8678388
apereira123456 <andrew.pereira123456@gmail.com>
2022-10-03 10:30:50 -0400

    update

--- Raman/Average_Raman_Spectra.m
+++ Raman/Average_Raman_Spectra.m
@@ -17,24 +17,14 @@ DataEditField.Value = file;
 
 data_name = fullfile(path, file);
 tic
-data = table2array(readtable(data_name));
+% data = table2array(readtable(data_name));
+data = readmatrix(data_name);
 toc
 
 
 %% Determine data array size
 tic
-[~,x,~] = unique(data(:,1),'rows');
-[~,y,~] = unique(data(:,2),'rows');
-[~,s,~] = unique(data(:,3),'rows');
-
-[x_size, ~] = size(x);
-[y_size, ~] = size(y);
-[s_size, ~] = size(s);
-toc
-
-tic
-s_start = data(1,3);
-s_size = find(data(2:end,3) == s_start, 1, 'first');
+s_size = find(data(2:end,3) == data(1,3), 1, 'first');
 
 x_size = (data(end,1) - data(1,1)) / (data(s_size + 1,1) - data(1,1)) + 1;
 y_size = (data(end,2) - data(1,2)) / (data(x_size*s_size + 1,2) - data(1,2)) + 1;
